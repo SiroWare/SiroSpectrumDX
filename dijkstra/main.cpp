bc13cc76c982cd5f4f4493dd7ab31ed66433d1fd
SimonR <sw.roeloffs@hotmail.com>
2025-05-27 17:25:15 +0200

    Update main.cpp

--- dijkstra/main.cpp
+++ dijkstra/main.cpp
@@ -9,9 +9,9 @@ SiroInput* input = input->PlugInKeyboard();
 SiroPencil* pencil = pencil->SharpenPencil();
 
 struct Entity {
-	Sprite* sprite;
 	unsigned char x;
 	unsigned char y;
+	unsigned char color;
 };
 
 class Maze : public Game {
@@ -19,12 +19,27 @@ public:
 	void setup() override;
 	void loop() override;
 
-	bool Collision(Entity* entity1, Entity* entity2);
+	unsigned char* LoadedMap;
+
+	void DecorateRoom(unsigned char* TileMap);
+
+	//bool Collision(Entity* entity1, Entity* entity2);
 
 };
 
+void Maze::DecorateRoom(unsigned char* TileMap) {
+	LoadedMap = TileMap;
+	unsigned short sum = 0;
+	for (int y = 0; y < 24; y++) {
+		for (int x = 0; x < 32; x++) {
+			pencil->DrawBGColour(x, y, LoadedMap[sum]);
+			sum++;
+		}
+	}
+}
 
-bool Maze::Collision(Entity* entity1, Entity* entity2) {
+
+/*bool Maze::Collision(Entity* entity1, Entity* entity2) {
 	if (entity1->x + entity1->sprite->width > entity2->x &&
 		entity1->x < entity2->x + entity2->sprite->width &&
 		entity1->y + entity1->sprite->height > entity2->y &&
@@ -33,12 +48,42 @@ bool Maze::Collision(Entity* entity1, Entity* entity2) {
 		return true;
 	}
 	return false;
-}
+}*/
 
-
+Entity* player = new Entity{ 3,6,RED };
+Entity* goal = new Entity{ 15,6,YLW };
 
 void Maze::setup() {
-
+	unsigned char map[32 * 24] = {
+		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+		0,0,0,0,0,0,0,0,0,0,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+		0,0,0,0,0,0,0,0,0,0,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+		0,0,0,0,0,0,0,0,0,0,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+		0,0,0,0,0,0,0,0,0,0,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+		0,0,0,0,0,0,0,0,0,0,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+		0,0,0,0,0,0,0,0,0,0,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+		0,0,0,0,0,0,0,0,0,0,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+		0,0,0,0,0,0,0,0,0,0,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+		0,0,0,0,0,0,0,0,0,0,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+		0,0,0,0,0,0,0,0,0,0,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	};
+	DecorateRoom(map);
+	pencil->DrawBGColour(player->x, player->y, player->color);
+	pencil->DrawBGColour(goal->x, goal->y, goal->color);
+	
 }
 
 void Maze::loop() {
